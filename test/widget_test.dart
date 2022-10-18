b0978e81f179a310543a248801a1503c19f90202
Mizerski <mizerskimachado@hotmail.com>
2022-10-17 22:26:48 -0300

    Docs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:divideai/main.dart';
+import 'package:divideai/main.dart'//
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
